049871186f2f5a37aacd925069a20279b74065c6
Thinh Le <55011202+ThinhLe514@users.noreply.github.com>
2020-03-03 16:16:55 -0500

    Add files via upload

--- Part-1/SavingsAccount.cpp
+++ Part-1/SavingsAccount.cpp
@@ -3,15 +3,25 @@
 using namespace std;
 
 // constructor initializes balance and interest rate
-SavingsAccount::SavingsAccount( double initialBalance, double rate ) : Account( initialBalance ) 
+SavingsAccount::SavingsAccount(double initialBalance, double rate) : Account(initialBalance)
 {
-	// your code
+    // your code
+    if (rate >= 0.0)
+        interestRate = rate * 100;
+    else
+        interestRate = 0.0;
 }
 
-double SavingsAccount::calculateInterest() {
-	// your code
-} 
-void SavingsAccount::display(ostream & os) const
+double SavingsAccount::calculateInterest()
 {
-	// your code
+    // your code
+    return getBalance() * interestRate;
+}
+void SavingsAccount::display(ostream &os) const
+{
+    // your code
+    os << fixed << setprecision(2);
+    os << "Account type: Saving" << endl;
+    os << "Balance: $" << getBalance() << endl;
+    os << "Interest Rate (%): " << interestRate << endl;
 }
