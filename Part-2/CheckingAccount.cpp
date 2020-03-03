cb03fedd1d66bbbbea5099afd9f1f163973a2342
Thinh Le <55011202+ThinhLe514@users.noreply.github.com>
2020-03-03 16:17:21 -0500

    Add files via upload

--- Part-2/CheckingAccount.cpp
+++ Part-2/CheckingAccount.cpp
@@ -7,27 +7,54 @@ using namespace std;
 CheckingAccount::CheckingAccount( double initialBalance, double fee ) : Account( initialBalance ) 
 {
 	// your code
+    if (fee >= 0.0)
+    {
+        transactionFee = fee;
+    } else 
+    {
+        transactionFee = 0.0;
+    }
 } 
 
 // credit (add) an amount to the account balance and charge fee
 void CheckingAccount::credit( double amount )
 {
 	// your code
+    Account::credit(amount);
+    chargeFee();
 } 
 
 // debit (subtract) an amount from the account balance and charge fee
 bool CheckingAccount::debit( double amount )
 {
 	// your code
+    bool check = Account::debit(amount);
+    if (check)
+    {
+        chargeFee();
+        return true;
+    }
+    return check;
 }
 
 // subtract transaction fee
 void CheckingAccount::chargeFee()
 {
 	// your code
+    if (getBalance() >= transactionFee)
+    {
+        setBalance(getBalance() - transactionFee);
+    } else
+    {
+        setBalance(0);
+    }
 }
 
 void CheckingAccount::display(ostream & os) const
 {
 	// your code
+    os << fixed << setprecision(2);
+    os << "Account type: Checking" << endl;
+    os << "Balance: $" << getBalance() << endl;
+    os << "Transaction Fee: $" << transactionFee << endl;
 }
